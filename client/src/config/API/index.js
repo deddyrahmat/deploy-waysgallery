80546dbbaad6fa6d14a3039a5c03515d77503442
deddyrahmat <45346578+deddyrahmat@users.noreply.github.com>
2021-03-11 09:43:55 +0700

    update data Baseurl API

--- client/src/config/API/index.js
+++ client/src/config/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "https://deploy-waysgallery.herokuapp.com/api/v1",
+    baseURL: "https://ways-gallery-app.herokuapp.com/api/v1",
 });
 
 export const setAuthToken = (token) => {
